9144e1b5355f4cc82dad0c1db74a1af1b1df860b
anto004 <anto_004@live.com>
2020-06-11 20:48:30 +0530

    Add HOC requireAuth for checking if user is authenticated

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,5 +1,8 @@
 import React from "react";
+import requireAuth from "./requireAuth";
 
-export default function Feature() {
+function Feature() {
 	return <h1>Feature Component</h1>;
 }
+
+export default requireAuth(Feature);
